ad27eb090055358911e77fce931008ef6d1ccc8f
Alvaro0305 <asr24gr036@student.esgerencia.com>
2026-05-22 14:39:15 +0200

    Ejercicio 3 Corregido

--- force-app/main/default/lwc/galeriaHome/galeriaHome.js
+++ force-app/main/default/lwc/galeriaHome/galeriaHome.js
@@ -1,38 +1,36 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import IMG1 from '@salesforce/resourceUrl/img1';
+import IMG2 from '@salesforce/resourceUrl/img2';
+import IMG3 from '@salesforce/resourceUrl/img3';
+import IMG4 from '@salesforce/resourceUrl/img4';
 
 export default class GaleriaHome extends LightningElement {
-    images = [
-        {
-            id: 1,
-            url: 'https://picsum.photos/id/1015/600/400'
-        },
-        {
-            id: 2,
-            url: 'https://picsum.photos/id/1025/600/400'
-        },
-        {
-            id: 3,
-            url: 'https://picsum.photos/id/1035/600/400'
-        },
-        {
-            id: 4,
-            url: 'https://picsum.photos/id/1045/600/400'
-        }
+
+    @track imagenes = [
+        { id: '1', url: IMG1, alt: 'Imagen 1', seleccionada: true },
+        { id: '2', url: IMG2, alt: 'Imagen 2', seleccionada: false },
+        { id: '3', url: IMG3, alt: 'Imagen 3', seleccionada: false },
+        { id: '4', url: IMG4, alt: 'Imagen 4', seleccionada: false },
     ];
 
-    selectedImage = 'https://picsum.photos/id/1015/600/400';
+    imagenPrincipal = IMG1;
 
-    handleImageClick(event) {
-        this.selectedImage = event.target.dataset.url;
+    get imagenesConClase() {
+        return this.imagenes.map(img => ({
+            ...img,
+            claseMinuatura: img.seleccionada ? 'thumbnail thumbnail-selected' : 'thumbnail'
+        }));
     }
 
-    get imageList() {
-        return this.images.map(image => ({
-            ...image,
-            cssClass:
-                image.url === this.selectedImage
-                    ? 'thumbnail selected'
-                    : 'thumbnail'
+    seleccionarImagen(event) {
+        const idSeleccionado = event.target.dataset.id;
+
+        this.imagenes = this.imagenes.map(img => ({
+            ...img,
+            seleccionada: img.id === idSeleccionado
         }));
+
+        const imagenEncontrada = this.imagenes.find(img => img.id === idSeleccionado);
+        this.imagenPrincipal = imagenEncontrada.url;
     }
 }
